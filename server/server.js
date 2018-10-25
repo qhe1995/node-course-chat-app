c5945adf7d4336a07f221870175ea32945f3eeff
Jason He <qhe1995@gmail.com>
2018-10-25 14:00:38 -0400

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,11 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newMessage', { 
-  //   from: 'jason@example.com', 
-  //   text: 'Nice to hear from you', 
-  //   createdAt: '123456'
-  // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
@@ -26,7 +33,12 @@ io.on('connection', (socket) => {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
